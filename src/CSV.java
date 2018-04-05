497c5a1f4ec7700a50f48582e524095697c1f75c
youssefadel94 <darkmetalxdragon@hotmail.com>
2018-04-05 13:46:30 +0200

    update and crete

--- src/CSV.java
+++ src/CSV.java
@@ -1,66 +1,74 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.io.PrintWriter; 
 
 public class CSV {
-	public static String[] columns;
-	public static int rowsCount;
-	public static String tableNameConst;
-	public static String tableColumnsConst;
-	public static PrintWriter pw;
-	public static StringBuilder sb;
-	public static int id;
-	public static int pages;
-	public static int pageRecords;
-	public static int rowLengh;
+	public static String[] columns; 
+	public static int rowsCount; 
+	public static String tableNameConst; 
+	public static String tableColumnsConst; 
+	public static PrintWriter pw; 
+	public static StringBuilder sb; 
+	public static int id; 
+	public static int pages; 
+	public static int pageRecords; 
+	public static int rowLengh; 
+	public static String[] tableColumnsCSV; 
 
 
 
 	public static void createTable(String tableName, String tableColumns)
 			throws FileNotFoundException {
 		
-		tableColumnsConst = tableColumns;//get table name
-		columns = tableColumns.split(", ");//get coloumns 
-		rowsCount = columns.length;//get count of rows
-		pw = new PrintWriter(new File(tableName + ".csv"));//open a new witer
-		sb = new StringBuilder();
-		for (int i = 0; i < rowsCount; i++) {
-			sb.append(columns[i]);sb.append(',');//add rows in the tbale head
+		tableColumnsConst = tableColumns; //get table name
+		columns = tableColumns.split(", "); //get coloumns 
+		rowsCount = columns.length; //get count of rows
+		pw = new PrintWriter(new File(tableName + ".csv")); //open a new witer
+		sb = new StringBuilder(); 
+		for (int i = 0; i < rowsCount; i ++ ) {
+			sb.append(columns[i]); sb.append(','); //add rows in the tbale head
 		}
-		sb.append('\n');
+		sb.append('\n'); 
 	}//test
 
-	public static void insertRecord(String row) throws FileNotFoundException {
+	public static void insertRecord(String row)throws FileNotFoundException {
 		
-		checkIfReachedMaxRecords();//check if reached max record to create a new page
+		checkIfReachedMaxRecords(); //check if reached max record to create a new page
 		//TODO Check if record is same as number of fields
-		String[] recordColumns = row.split(", ");//get columns to be added
-		int rowLengh = recordColumns.length;
-		sb.append(id);sb.append(',');
+		String[] recordColumns = row.split(", "); //get columns to be added
+		int rowLengh = recordColumns.length; 
+		sb.append(id); sb.append(','); 
 		//if (rowLengh + 1 <= rowsCount) {//TODO take this check out in can add
-			if(canAdd(row))
-				System.out.print("hey");
-		      for (int i = 0; i < rowLengh; i++) {
-				sb.append(recordColumns[i]);sb.append(',');//add record
+			if (canAdd(row))
+				System.out.print("hey"); 
+		      for (int i = 0; i < rowLengh; i ++ ) {
+				sb.append(recordColumns[i]); sb.append(','); //add record
 			}
-			sb.append('\n');
-			id++;//increment id of records
-			pageRecords++;//increment page records counter
+			sb.append('\n'); 
+			id ++ ; //increment id of records
+			pageRecords ++ ; //increment page records counter
 		}
 	
 
-	public static String readFromCSV(){
+	public static String[] readFromCSV() {
 		//TODO Function read all csv files and generate a sting  
-		return tableColumnsConst;
+		return tableColumnsCSV; 
 		//TODO brin index load and save for searching
 	}
 	//TODO brin library 
-	public static void update(int id, String record){
-		String records = readFromCSV();//see if lookup is by id or name
+	public static Boolean update(int id, String record) throws FileNotFoundException{
+		String records[] = readFromCSV(); //see if lookup is by id or name
+		for (int i = 2; i <= records.length; i ++ ) {//check for condition
+			if (records[i].contains(Integer.toString(id))) {
+				records[i] = record; 
+				createCSV(records); 
+				return true; 
+			}
+		}return false; 
 		//TODO Check if record exist then update it in the string and add it to the csv
 	}
-	public static boolean canAdd(String row){
-		boolean f= false;
+	public static boolean canAdd(String row) {
+		boolean f = false; 
 		//TODO Check if record can be add and dosn't exceed field numbers
 		String[] recordColumns = row.split(",");//get columns to be added
 		rowLengh = recordColumns.length;
@@ -73,7 +81,7 @@ public class CSV {
 		return f; 
 	}
 	public static void delete(int id){
-		String records = readFromCSV();//see if lookup is by id or name
+		String[] records = readFromCSV();//see if lookup is by id or name
 		//TODO Check if record exist then update it in the string and add it to the csv
 	}
 	
@@ -87,7 +95,12 @@ public class CSV {
 			pageRecords = 0;
 		}
 	}
-
+public static void createCSV(String[] records) throws FileNotFoundException{
+	createTable(records[0], records[1]);
+	for(int i =2;i<=records.length;i++){
+		insertRecord(records[i]);
+	}
+}
 	public static void main(String[] args) throws FileNotFoundException {
 		id = 0;
 		pages = 1;
